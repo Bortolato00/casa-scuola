5025b1a3166d0d2871594698345556cbb4fc390c
Bortolato00 <michelangelo.bortolato@gmail.com>
2017-10-09 11:59:23 +0200

    Compiti

---
+++ Compiti171011.cpp
@@ -0,0 +1,9 @@
+ #include <iostream>
+ using namespace std;
+ class calcolatrice{
+ public:
+    double somma(double a, double b);
+    double differenza(double a, double b);
+    double quoziente(double a, double b);
+    double prodotto (double a, double b);
+ };
